b9b935532f76c20f8c752a4c8594e62ff19ec613
Emmanuel Moncada <moncadae@hsd.k12.or.us>
2026-09-04 15:32:43 -0700

    clean whitespace

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { employees } from "./data";
 export default function App() {
   return (
     <div className="flex flex-col items-center py-4 px-2 w-full max-w-full overflow-x-hidden">
-
       <h1 className="text-3xl mb-2">Hillsboro School District - Facilities Trade Crews</h1>
       <h2 className="text-2xl mb-6">Org Chart</h2>
       <OrgChart employees={employees} />
